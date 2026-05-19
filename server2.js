eb89b4b09523b9db42812ba9e5947f04da127ede
derekonsio1023-gif <derekonsio1023@gmail.com>
2026-05-19 23:10:44 +0200

    Update and rename server.js to server2.js

---
+++ server2.js
@@ -0,0 +1,37 @@
+const express = require('express');
+const cors = require('cors');
+const ytdl = require('@distube/ytdl-core');
+
+const app = express();
+app.use(cors());
+
+app.get('/', (req, res) => res.send('OK'));
+app.get('/health', (req, res) => res.send('healthy'));
+
+app.get('/stream', async (req, res) => {
+    const videoId = req.query.id;
+
+    if (!videoId) {
+        return res.status(400).send('Falta el ID del vídeo');
+    }
+
+    const videoUrl = `https://youtube.com/watch?v=${videoId}`;
+
+    try {
+        res.setHeader('Content-Type', 'video/mp4');
+
+        ytdl(videoUrl, {
+            filter: 'audioandvideo',
+            quality: 'highest'
+        }).pipe(res);
+
+    } catch (error) {
+        console.error(error);
+        res.status(500).send('Error al procesar el vídeo');
+    }
+});
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+    console.log(`Servidor proxy corriendo en puerto ${PORT}`);
+});
